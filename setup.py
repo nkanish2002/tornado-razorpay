f9d647ab16365674499ece585e71a4e7a252d1d5
Anish <nkanish2002@gmail.com>
2016-06-10 13:26:07 +0530

    Bumping the version

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from sys import version_info, exit
 from setuptools import setup, find_packages
+from os import path
 
 if version_info[0] < 3 and version_info[1] < 5:
     exit("Sorry, support only for Python 3.5 and above.")
 
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="tornado-razorpay",
-    version="0.1.2",
+    version="0.1.3",
     description="Razorpay Asynchronous Tornado Python Client",
     url="https://github.com/nkanish2002/tornado-razorpay",
     author="Anish Gupta",
